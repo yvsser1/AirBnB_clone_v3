01c9d80a4ec0d666656bc2275ce680cc99680d4b
Yasser <yasserdalouzi@gmail.com>
2024-08-06 12:26:16 +0100

    error fix

---
+++ test_get_count.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Test .get() and .count() methods
+"""
+from models import storage
+from models.state import State
+
+state = State(name='Rabat')
+storage.new(state)
+storage.save
+
+print("All objects: {}".format(storage.count()))
+print("State objects: {}".format(storage.count(State)))
+
+first_state_id = list(storage.all(State).values())[0].id
+print("First state: {}".format(storage.get(State, first_state_id)))
